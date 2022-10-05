5669a03d971f6647bed6e1656889bbe06221eda0
Limpet-z <695612981anthony@gmail.com>
2022-10-05 11:11:40 +0300

    Initial commit

---
+++ src/main/java/root/web3j/core/BalancePersonalAcc.java
@@ -0,0 +1,35 @@
+package root.web3j.core;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.DefaultBlockParameter;
+import org.web3j.protocol.core.methods.response.EthGetBalance;
+import org.web3j.protocol.http.HttpService;
+
+
+import java.math.BigInteger;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+public class BalancePersonalAcc  {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException, TimeoutException {
+
+        Logger log = LoggerFactory.getLogger(BalancePersonalAcc.class);
+
+        Web3j client = Web3j.build(new HttpService("https://mainnet.infura.io/v3/cd601df89a60461f9d744777d13769a5"));
+        Web3j client2 = Web3j.build(new HttpService("https://api.myetherapi.com/eth"));
+        Web3j client3 = Web3j.build(new HttpService());
+
+        final String ethAddress = "0xb69749D19517b0255985C1a6eCDD05a6307dC30E";
+        final EthGetBalance balanceResponse = client
+                .ethGetBalance(ethAddress, DefaultBlockParameter.valueOf("latest")).sendAsync()
+                .get(10, TimeUnit.SECONDS);
+
+        final BigInteger unscaledBalance = balanceResponse.getBalance();
+
+        log.info("Some object: {}", unscaledBalance);
+    }
+}
